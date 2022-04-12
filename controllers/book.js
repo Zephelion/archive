c8f398d98a3e819fc4cce4a6242a85909e2dd10f
Zephelion <russellnumo@outlook.com>
2022-04-12 17:58:24 +0200

    book is saved in db

--- controllers/book.js
+++ controllers/book.js
@@ -1,15 +1,36 @@
-const res = require('express/lib/response');
-const Book = require('../models/Book');
+const Books = require('../models/Book');
 
 const index = (req,res) => {
-    res.render('index');
+    Books.find().lean().then(books => {
+        res.render('index', {
+            books:books,
+        });
+    });
 }
 
 const store = (req,res) => {
     res.render('store');
 }
 
+const save = (req,res) => {
+    const form = {
+		title: req.body.title,
+		author: req.body.author,
+		catalogue_number: req.body.catalogue_number,
+		year_publication: req.body.year_publication,
+		place_publication: req.body.place_publication,
+		language: req.body.language,
+		subject: req.body.subject
+	};
+
+    const book = new Books(form);
+    book.save();
+
+    res.redirect('/')
+}
+
 module.exports = {
     index:index,
-    store:store
+    store:store,
+    save:save
 }
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const book = require('../controllers/book');
 
 router.get('/', book.index);
 router.get('/store', book.store);
+router.post('/store', book.save);
 
 
 module.exports = router;
