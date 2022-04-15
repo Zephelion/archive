52bd029282906ab20d636455fd682183df2fd2d3
Zephelion <russellnumo@outlook.com>
2022-04-15 16:25:03 +0200

    working on search still buggin

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,6 @@
 const searchInput = document.getElementById("search");
+const bookContainer = document.getElementById("book-container");
+let bookHTML = '';
 
 searchInput.addEventListener("keyup", (e) =>{
     	
@@ -7,7 +9,30 @@ searchInput.addEventListener("keyup", (e) =>{
     console.log(value);
     axios.post('/search', {value:value})
     .then(function (response){
-        console.log(response.data);
+        const books = response.data
+        
+        books.forEach(book => {
+            bookHTML += `                        
+            <div class="py-8 flex flex-wrap md:flex-nowrap">
+                <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span class="font-semibold title-font text-gray-700">Author</span>
+                <span class="mt-1 text-gray-500 text-sm">${book.author}</span>
+                </div>
+                <div class="md:flex-grow">
+                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">${book.title}</h2>
+                <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+                <p class="leading-relaxed font-bold">${book.catalogue_number}</p>
+                <a href="#" class="text-indigo-500 inline-flex items-center mt-4">Learn More
+                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                </a>
+                </div>
+            </div>`
+        });
+        bookContainer.innerHTML = bookHTML;
+
     }).catch(function(error){
         console.log(error);
     })
